9a83206c635b465db6e2d79106c4995086b89326
cbgoodrich <31485791+cbgoodrich@users.noreply.github.com>
2017-11-15 13:56:16 -0500

    Updated from Brython Server: 11/15/2017 1:56:15 PM

--- stat.py
+++ stat.py
@@ -1,1 +1,12 @@
+#Charlie Goodrich
+#11/15/17
+#stat.py - does statistical analysis of inputted numbers
 
+print("Type a list of numbers")
+print("Enter 'q' when finished")
+numbers = []
+while True:
+    num = float(input("> "))
+    numbers.append(num)
+    
+    
