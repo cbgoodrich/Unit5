a67c70e4cab8650ae49bc5493cf7b688f9fec286
cbgoodrich <31485791+cbgoodrich@users.noreply.github.com>
2017-11-16 14:16:54 -0500

    Updated from Brython Server: 11/16/2017 2:16:52 PM

--- warmup13.py
+++ warmup13.py
@@ -3,3 +3,13 @@
 #warmup13.py - makes 20 random #'s gives min, max, and sum
 
 from random import randint
+
+numbers = []
+i = 2
+
+while i <= 21:
+    num = randint(1, i)
+    numbers.append(num)
+    i += 1
+    
+print(numbers[5])
