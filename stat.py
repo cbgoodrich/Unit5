13a1c4717d417b1aa7e9424bc71cfd36c7bf922b
cbgoodrich <31485791+cbgoodrich@users.noreply.github.com>
2017-11-16 14:36:53 -0500

    Updated from Brython Server: 11/16/2017 2:36:52 PM

--- stat.py
+++ stat.py
@@ -14,9 +14,22 @@ while True:
         
 numbers.sort()
 
+c = 0
+i = 0
+for n in numbers:
+    while i < len(numbers):
+        count = numbers.count(numbers[i])
+        if count > c:
+            c = count
+            mode = numbers[i]
+        if c == 1 and len(numbers) > 1:
+            mode = "None"
+        i += 1
+
 print("Min:", numbers[0])
 print("Max:", numbers[-1])
 print("Mean:", sum(numbers)/len(numbers))
+print("Mode:", mode)
 
 
     
