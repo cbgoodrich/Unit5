66635de856785ff6fa6407866e1e8ae3d3a0ef2c
cbgoodrich <31485791+cbgoodrich@users.noreply.github.com>
2017-11-16 14:54:42 -0500

    Updated from Brython Server: 11/16/2017 2:54:41 PM

--- toDoList.py
+++ toDoList.py
@@ -1,1 +1,18 @@
+#Charlie Goodrich
+#11/16/17
+#toDoList.py - makes a to do list that you can add stuff to
 
+toDoList = []
+validCommands = "add, delete, print, quit"
+print("Valid commands are:", validCommands)
+
+while True:
+    toDo = input("> ")
+    if toDo[0:2] == "add":
+        toDoList.append(toDo[4:])
+    elif toDo == "done":
+        break
+    
+print(toDoList[:])
+    
+    
