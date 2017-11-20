194ba04d93cc9554da06f3d9022ed5ce0bfbdea9
cbgoodrich <31485791+cbgoodrich@users.noreply.github.com>
2017-11-20 14:50:16 -0500

    Updated from Brython Server: 11/20/2017 2:50:15 PM

--- snow.py
+++ snow.py
@@ -3,21 +3,24 @@
 #snow.py - makes snow fall and accumulates on the ground
 
 from ggame import *
+from random import randint
 
 #CONSTANTS
 WINDOW_X = 1000
 WINDOW_Y = 520
 CELL_SIZE = 10
 
+def spawnSnow():
+    data["snowList"].append(Sprite(snow,(randint(0, WINDOW_X))))
+
 def moveSnow():
-    snow.y += CELL_SIZE
-    data["frames"] = 0
-    if snow.y >= WINDOW_Y-CELL_SIZE:
-        data["frames"] = 6
+    for snow in data["snowList"]:
+        flake.y += 5
 
 def step():
     data["frames"] += 1
     if data["frames"] == 5:
+        spawnSnow()
         moveSnow()
 
 if __name__ == "__main__":
@@ -25,23 +28,17 @@ if __name__ == "__main__":
     snowList = []
     
     data = {}
-    data["frames"] = 0
+    data["snowList"] = []
+    
     
     white = Color(0xFFFFFF, 1)
     blue = Color(0x000080, 1)
     
-    whiteBox = RectangleAsset(CELL_SIZE, CELL_SIZE, LineStyle(1, white), white)
+    snow = RectangleAsset(CELL_SIZE, CELL_SIZE, LineStyle(1, white), white)
     sky = RectangleAsset(WINDOW_X, WINDOW_Y, LineStyle(1, blue), blue)
     
     Sprite(sky)
-    i = 0
-    while True:
-        snow = Sprite(whiteBox, (i, 0))
-        snowList.append(snow.y)
-        i += CELL_SIZE
-        if i >= 1000:
-            break
-        
+    
     
     
     
