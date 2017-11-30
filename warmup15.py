4dadb199254b2489d7ded619080c3144f34f9f94
cbgoodrich <31485791+cbgoodrich@users.noreply.github.com>
2017-11-30 14:10:41 -0500

    Updated from Brython Server: 11/30/2017 2:10:40 PM

--- warmup15.py
+++ warmup15.py
@@ -1,1 +1,20 @@
+#Charlie Goodrich
+#11/30/17
+#warmup15.py - takes a list of numbers then multiplies them by two
 
+A = []
+
+def doubled(A):
+    while True:
+        num = input("Enter a number, or enter stop to stop: ")
+        if num == "stop":
+            break
+        else:
+            A.append(int(num))
+    
+    for num in A:
+        num *= 2
+        
+    return A
+    
+print(doubled[2,4,6])
